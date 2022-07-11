be8f6d53111b7c73c4b34f9059443a603cdf9f9c
Jmontenar <jmontenar@gmail.com>
2022-07-10 23:19:56 -0500

    Detalles

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,7 +4,6 @@ const heroMenu = document.getElementById('heroMenu')
 const sections = document.querySelectorAll('section[id]')
 const form = document.getElementById('form')
 
-/* Header agregar/quitar fondo */
 window.addEventListener('scroll', function () {
   if (window.scrollY >= 60) {
     navBar.classList.add('nav-fixed')
@@ -12,8 +11,6 @@ window.addEventListener('scroll', function () {
     navBar.classList.remove('nav-fixed')
   }
 })
-
-/* Menu Navbar */
 btnMenu.addEventListener('click', function () {
   heroMenu.classList.toggle('active')
 })
@@ -21,5 +18,41 @@ btnMenu.addEventListener('click', function () {
 heroMenu.addEventListener('click', function (e) {
   if (e.target.matches('.hero__nav-link')) {
     heroMenu.classList.remove('active')
+  }
+})
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const email = 'jmontenar@gmail.com'
+  const URL_BASE = `https://formsubmit.co/${email}`
+
+  const input = e.currentTarget.elements
+  const formData = {
+    name: input.name.value,
+    email: input.email.value,
+    message: input.message.value
+  }
+
+  const options = {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(formData)
+  }
+
+  fetch(URL_BASE, options)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+  try {
+    const res = await fetch(URL_BASE, options)
+    const data = await res.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
   }
 })
